67f564df8bb80ec2966d8101e0b86e76704fd21d
sxu01 <shao.xu@mlb.com>
2015-12-24 14:41:49 -0500

    Add initial project structure

---
+++ mainJobs/src/main/java/com/chinese/jobs/App.java
@@ -0,0 +1,13 @@
+package com.chinese.jobs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
